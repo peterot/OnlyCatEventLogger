39aa95c6db2fd0da40ee23ca6a80c351b32273a2
peterot <peterot@gmail.com>
2025-12-23 14:13:56 +0000

    Inproved Ack logging

--- src/main/java/com/onlycat/ingest/onlycat/SocketIoOnlyCatClient.java
+++ src/main/java/com/onlycat/ingest/onlycat/SocketIoOnlyCatClient.java
@@ -15,6 +15,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -160,15 +161,31 @@ public class SocketIoOnlyCatClient implements OnlyCatClient, ApplicationRunner, 
         if (!StringUtils.hasText(event)) {
             return;
         }
-        log.info("Emitting smoke-test event '{}'", event);
-        socket.emit(event);
+        log.info("Emitting smoke-test event '{}' (ack will be logged)", event);
+        socket.emit(event, (Object) null, (Ack) args -> {
+            log.info("Ack for '{}': {}", event, Arrays.toString(args));
+            maybeDumpAckJson(event, args);
+        });
         if (properties.getSubscribeEvents() != null) {
             for (String sub : properties.getSubscribeEvents()) {
                 if (StringUtils.hasText(sub)) {
-                    log.info("Emitting subscription event '{}'", sub);
-                    socket.emit(sub);
+                    log.info("Emitting subscription event '{}' (ack will be logged)", sub);
+                    socket.emit(sub, (Ack) args -> {
+                        log.info("Ack for subscription '{}': {}", sub, Arrays.toString(args));
+                        maybeDumpAckJson(sub, args);
+                    });
                 }
             }
+        }
+    }
+
+    private void maybeDumpAckJson(String event, Object[] args) {
+        try {
+            Object payload = (args != null && args.length == 1) ? args[0] : args;
+            String json = new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(payload);
+            log.info("Ack payload JSON for '{}': {}", event, json);
+        } catch (Exception e) {
+            log.debug("Ack payload for '{}' not JSON-serializable: {}", event, Arrays.toString(args));
         }
     }
 
